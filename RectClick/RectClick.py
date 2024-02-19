621f866efd08fc37e1342520bde26fb58cbea6d1
Timotej Ružička <102914473+TimotejR2@users.noreply.github.com>
2024-02-19 17:55:48 +0100

    Add files via upload

--- RectClick/RectClick.py
+++ RectClick/RectClick.py
@@ -2,23 +2,21 @@ import tkinter as tk
 import random
 
 root = tk.Tk()
-game_canvas = tk.Canvas(root)
-game_canvas.pack()
+canvas = tk.Canvas(root)
+canvas.pack()
 
 player_score = 0
 
 # Create game environment
 for i in range(0, 500, 30):
-    game_canvas.create_line(0, i, 500, i)
-    game_canvas.create_line(i, 0, i, 500)
+    canvas.create_line(0, i, 500, i)
+    canvas.create_line(i, 0, i, 500)
 
-game_canvas.create_rectangle(0, 0, 30, 30, fill="blue", tag="correct")
-correct_rect_x = 0
-correct_rect_y = 0
+canvas.create_rectangle(0, 0, 30, 30, fill="blue", tag="correct")
+correct_rect_x, correct_rect_y = 0, 0
 
-game_canvas.create_rectangle(0, 0, 30, 30, fill="red", tag="incorrect")
-incorrect_rect_x = 0
-incorrect_rect_y = 0
+canvas.create_rectangle(0, 0, 30, 30, fill="red", tag="incorrect")
+incorrect_rect_x, incorrect_rect_y = 0, 0
 
 def move_rectangles():
     global correct_rect_x, correct_rect_y, incorrect_rect_x, incorrect_rect_y
@@ -28,13 +26,12 @@ def move_rectangles():
     incorrect_rect_x = random.randrange(0, 360, 30)
     incorrect_rect_y = random.randrange(0, 270, 30)
     
-    game_canvas.coords("correct", correct_rect_x, correct_rect_y, correct_rect_x + 30, correct_rect_y + 30)
+    canvas.coords("correct", correct_rect_x, correct_rect_y, correct_rect_x + 30, correct_rect_y + 30)
+    canvas.coords("incorrect", incorrect_rect_x, incorrect_rect_y, incorrect_rect_x + 30, incorrect_rect_y + 30)
 
-    game_canvas.coords("incorrect", incorrect_rect_x, incorrect_rect_y, incorrect_rect_x + 30, incorrect_rect_y + 30)
+    canvas.update()
 
-    game_canvas.update()
-
-    game_canvas.after(1000 - player_score * 50, move_rectangles)
+    canvas.after(1000 - player_score * 50, move_rectangles)
 
 move_rectangles()
 
@@ -43,7 +40,7 @@ def handle_click(event):
     mid_x, mid_y = calculate_mid_point(event.x, event.y)
 
     if is_collision(mid_x, mid_y, incorrect_rect_x, incorrect_rect_y):
-        game_canvas.quit()
+        canvas.quit()
     elif is_collision(mid_x, mid_y, correct_rect_x, correct_rect_y):
         player_score += 1
     else:
@@ -56,6 +53,5 @@ def calculate_mid_point(x, y):
 def is_collision(x1, y1, x2, y2):
     return x1 == x2 and y1 == y2
 
-
-game_canvas.bind("<Button-1>", handle_click)
+canvas.bind("<Button-1>", handle_click)
 root.mainloop()
